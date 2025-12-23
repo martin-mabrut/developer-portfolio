c256cdbe7c0c09cb61a022eb4598bd94d26b2fe1
Martin Mabrut <martinmabrut43@gmail.com>
2025-12-23 09:25:56 +0100

    fix images paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: "https://martin-mabrut.github.io",
-  base: "/portfolio",
+  site: 'https://martin-mabrut-portfolio.netlify.app'
 });
